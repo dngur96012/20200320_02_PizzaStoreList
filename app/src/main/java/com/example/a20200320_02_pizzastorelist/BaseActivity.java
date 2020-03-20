c63465d874fd4cb9110ca9f57264d614cefc1517
dngr96012 <dngurdl123!>
2020-03-20 11:46:49 +0900

    피자 가게 데이

---
+++ app/src/main/java/com/example/a20200320_02_pizzastorelist/BaseActivity.java
@@ -0,0 +1,4 @@
+package com.example.a20200320_02_pizzastorelist;
+
+public class BaseActivity {
+}
---
+++ app/src/main/java/com/example/a20200320_02_pizzastorelist/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.a20200320_02_pizzastorelist;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+
+import com.example.a20200320_02_pizzastorelist.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+    }
+}
